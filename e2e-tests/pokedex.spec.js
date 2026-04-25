0b3d5a68896f07ad2184df9fc3445ea1b3060a28
saman nakarmi <samann@metropolia.fi>
2026-04-25 12:37:40 +0300

    setup CI/CD

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,15 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test');
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('');
+    await expect(page.getByText('ivysaur')).toBeVisible();
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
+  });
+
+  test('navigation to ivysaur page works', async ({ page }) => {
+    await page.goto('');
+    await page.click('text=ivysaur');
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+  });
+});
---
+++ playwright.config.js
@@ -0,0 +1,27 @@
+// playwright.config.js
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm start',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
+});
