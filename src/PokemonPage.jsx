aa97abc019086e8a435f7eb338b37b703fde82d4
saman nakarmi <samann@metropolia.fi>
2026-04-27 23:13:30 +0300

    fix next pokemon link

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -14,30 +14,40 @@ const PokemonPage = ({ previous, next }) => {
   if (isLoading) {
     return <LoadingSpinner />
   }
+
   if (error) {
     return <ErrorMessage error={error} />
   }
 
   const { type } = pokemon.types.find((type) => type.slot === 1)
-  const stats = pokemon.stats.map((stat) => ({
-    name: formatName(stat.stat.name),
-    value: stat.base_stat
-  })).reverse()
-  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
-  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
-  console.log('hiddenAbility=', hiddenAbility)
+  const stats = pokemon.stats
+    .map((stat) => ({
+      name: formatName(stat.stat.name),
+      value: stat.base_stat
+    }))
+    .reverse()
+
+  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
+  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden)
+
   return (
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>} {/* ✅ FIXED HERE */}
       </div>
+
       <div className={`pokemon-page pokemon-type-${type.name}`}>
-        <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
+        <div
+          className="pokemon-image"
+          style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }}
+        />
+
         <div className="pokemon-info">
           <div className="pokemon-name">{pokemon.name}</div>
+
           <div className="pokemon-stats" data-testid="stats">
             <table>
               <tbody>
@@ -50,9 +60,14 @@ const PokemonPage = ({ previous, next }) => {
               </tbody>
             </table>
           </div>
+
           <div className="pokemon-abilities">
-            {normalAbility && <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />}
-            {hiddenAbility && <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />}
+            {normalAbility && (
+              <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />
+            )}
+            {hiddenAbility && (
+              <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />
+            )}
           </div>
         </div>
       </div>
